b7b4a2c7f74a9c01c756cd11d13271257440df4a
John Barry <cellarise@outlook.com>
2014-07-13 16:54:09 +1000

    MAR-49 #time 0h Add grunt task from https://github.com/dprentis/grunt-ez-istanbul and update readme.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Cellarise-Istanbul
 This package has been forked from gotwarlost/istanbul, and updates the clover report to provide reporting of total source lines, so that the report provides accurate reporting of total lines versus source code lines.
 
+This package also includes the grunt-ez-istanbul package - a grunt plugin for Istanbul command line functionality (version: 0.0.4; author: Daniel Prentis; license: MIT; url: https://github.com/dprentis/grunt-ez-istanbul).
+
 ## Istanbul - a JS code coverage tool written in JS
 
 [![Build Status](https://secure.travis-ci.org/gotwarlost/istanbul.png)](http://travis-ci.org/gotwarlost/istanbul)
---
+++ tasks/istanbul.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+
+  grunt.registerMultiTask('istanbul', 'Run Istanbul commands', function () {
+
+    var options = this.options(), file = options.file, fileArgs = options.fileArgs,
+      cmdArgs = [options.command || 'help'], done = this.async(),
+      path = require('path'), fork = require('child_process').fork;
+
+    delete options.file;
+    delete options.fileArgs;
+    delete options.command;
+
+    for(var key in options) {
+      if (options.hasOwnProperty(key)) {
+        var arg = options[key];
+        if (typeof arg === 'boolean') {
+          if (arg) cmdArgs.push('--' + key);
+        } else {
+          cmdArgs.push('--' + key);
+          cmdArgs.push(String(arg));
+        }
+      }
+    }
+
+    if (file) {
+      cmdArgs.push(file);
+      if (fileArgs) {
+        cmdArgs.push('--');
+        cmdArgs = cmdArgs.concat(fileArgs.split(' '));
+      }
+    }
+
+    fork(path.normalize(__dirname + './lib/cli.js'), cmdArgs)
+      .on('exit', function (code, signal) {
+        done(code === 0 ? true : false);
+      });
+  });
+};
